2c762fe90e20fbe0a7b8885e2c4d17e475539f89
René Pacios <rene15009@hotmail.com>
2019-10-21 18:36:42 +0200

    aa

---
+++ 10.StaticWebSite/js/a1.js
@@ -0,0 +1,43 @@
+﻿"use strict";
+
+/** @description Determina la suma de los términos.
+ * @param {number} a Termino a sumar
+ * @param {number} b Termino a sumar
+ * @param {number} c Termino a sumar
+ * @return {number} Suma de los términos
+ */
+function sumar(a, b, c) {
+    
+    return a + b;
+}
+
+/** @description Determina la resta de los términos.
+ * @param {number} a Termino a sumar
+ * @param {number} b Termino b sumar
+ * @return {number} Resta de los términos
+ */
+function resta(a, b) {
+
+    return a - b;
+}
+
+
+/** @description Determina la multiplicar de los términos.
+ * @param {number} a Termino a sumar
+ * @param {number} b Termino b sumar
+ * @return {number} Multiplicación de los términos
+ */
+function multiplica(a, b) {
+
+    return a * b;
+}
+
+/** @description Determina la división de los términos.
+ * @param {number} a Termino 
+ * @param {number} b Termino 
+ * @return {number} Suma de los términos
+ */
+function dividir(a, b) {
+
+    return a / b;
+}
