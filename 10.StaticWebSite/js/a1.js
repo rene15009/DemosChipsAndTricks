0680d7cd072d3a4bc06277dfbe2babac402637db
René Pacios <rene15009@hotmail.com>
2019-10-22 21:32:52 +0200

    refactorizando

--- 10.StaticWebSite/js/a1.js
+++ 10.StaticWebSite/js/a1.js
@@ -1,43 +1,43 @@
 ﻿"use strict";
 
 /** @description Determina la suma de los términos.
- * @param {number} a Termino a sumar
- * @param {number} b Termino a sumar
- * @param {number} c Termino a sumar
+ * @param {number} termino Termino a sumar
+ * @param {number} termino2 Termino a sumar
+ * @param {number} termino3 Termino a sumar
  * @return {number} Suma de los términos
  */
-function sumar(a, b, c) {
+function sumar(termino, termino2, termino3) {
     
-    return a + b;
+    return termino + termino2;
 }
 
 /** @description Determina la resta de los términos.
- * @param {number} a Termino a sumar
- * @param {number} b Termino b sumar
+ * @param {number} minuendo Termino a restar
+ * @param {number} substraendo Termino b restar
  * @return {number} Resta de los términos
  */
-function resta(a, b) {
+function resta(minuendo, substraendo) {
 
-    return a - b;
+    return minuendo - substraendo;
 }
 
 
 /** @description Determina la multiplicar de los términos.
- * @param {number} a Termino a sumar
- * @param {number} b Termino b sumar
+ * @param {number} factor Termino a sumar
+ * @param {number} factor2 Termino b sumar
  * @return {number} Multiplicación de los términos
  */
-function multiplica(a, b) {
+function multiplica(factor, factor2) {
 
-    return a * b;
+    return factor * factor2;
 }
 
 /** @description Determina la división de los términos.
- * @param {number} a Termino 
- * @param {number} b Termino 
+ * @param {number} dividendo Termino 
+ * @param {number} divisor Termino 
  * @return {number} Suma de los términos
  */
-function dividir(a, b) {
-
-    return a / b;
+function dividir(dividendo, divisor) {
+    
+    return !!divisor && (dividendo / divisor);
 }
